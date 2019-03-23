9b05c84346b94e8d3d33c704ae511b164c766d4a
DMcRae45 <DMcRae45@gmail.com>
2019-03-23 00:09:02 +0000

    fixed some layout issues and split up the paypal files.

---
+++ Controller/payPalButton.php
@@ -0,0 +1,42 @@
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<script>
+paypal.Button.render({
+    // Configure environment
+    env: '<?php echo $paypal->paypalEnv; ?>',
+    client: {
+        sandbox: '<?php echo $paypal->paypalClientID; ?>',
+        production: '<?php echo $paypal->paypalClientID; ?>'
+    },
+    // Customize button (optional)
+    locale: 'en_GB',
+    style:
+    {
+        size: 'large',
+        color: 'gold',
+        textarea: 'white',
+        shape: 'pill'
+    },
+    // Set up a payment
+    payment: function (actions) {
+        return actions.payment.create({
+            transactions: [{
+                amount: {
+                    total: '<?php echo $totalCost?>',
+                    currency: 'GBP'
+                }
+            }]
+      });
+    },
+
+    // Execute the payment
+    onAuthorize: function (data, actions) {
+        return actions.payment.execute()
+        .then(function () {
+            // Show a confirmation message to the buyer
+            window.alert('Thank you for your purchase!');
+            // Redirect to the payment process page
+            window.location = "../Controller/process.php?paymentID="+data.paymentID+"&token="+data.paymentToken+"&payerID="+data.payerID;
+        });
+    }
+}, '#paypal-button');
+</script>
--- View/checkout.php
+++ View/checkout.php
@@ -10,39 +10,43 @@ include '../Model/PaypalExpress.php';
 $paypal = new PaypalExpress;
 ?>
 
-<html lang="en-US">
-<head>
-<script src="https://www.paypalobjects.com/api/checkout.js"></script>
-</head>
 <body>
 
 <?php
   echo "<div class='container'>"; // Open container
     echo "<div class='mt-4'>";
       echo "<h3 class='d-inline'>".$movieArray->Title."</h3>"; // Display movie title
-      echo "<img class='d-inline float-right' src=".$movieArray->Age_Rating." class='img-fluid' style='height: 3rem'>";
+      echo "<img class='ml-3' src=".$movieArray->Age_Rating." class='img-fluid' style='height: 3rem'>";
     echo "</div>";
-      echo "<div class='row mt-4'>";
-        echo "<div class='col-md-6'>";
+      echo "<div class='row mt-4 mx-auto'>";
+        echo "<div class='col-md-3'>";
           echo "<p>Showing At: <text>Dundee Picture House</text></p>";
-          echo "<hr>";
-          echo "<p>Date: <text>".$showingDateString."</text></p>";
           echo "<hr>";
           echo "<p>Starting At: <text>".$showingTime."</text></p>";
           echo "<hr>";
           echo "<p>Showing In: <text>".$showingType."</text></p>";
           echo "<hr>";
+        echo "</div>";
+
+        echo "<div class='col-md-3'>";
+          echo "<p>Date: <text>".$showingDateString."</text></p>";
+          echo "<hr>";
+          echo "<p>RunTime: <text>".$movieArray->RunTime."</text></p>";
+          echo "<hr>";
           echo "<p>Screen: <text>".$showingArray->Screen_ID."</text></p>";
           echo "<hr>";
+        echo "</div>";
 
+        echo "<div class='col-md-6'>";
+        echo "<img src=".$movieArray->Image_Link." class='mx-auto d-block' style='height: 20rem;' onerror=this.src='images/film.placeholder.poster.jpg'>";
         echo "</div>";
       echo "</div>";
 
   echo "</div>"; // Close container
 
-echo "<div class= 'container'>";
+  echo "<div class= 'container'>";
     echo "
-      <table class='table table-dark'>
+      <table class='table table-dark mt-3'>
         <thead>
           <tr>
           <th scope='col'>Ticket</th>
@@ -50,7 +54,7 @@ echo "<div class= 'container'>";
           <th scope='col'>Sub-Total</th>
           </tr>
         </thead>
-        <body>";
+        <tbody>";
 
     for ($i=0 ; $i < sizeof($quantityArray) ; $i++)
      {
@@ -84,54 +88,15 @@ echo "<div class= 'container'>";
             <tr>
         </tbody>
       </table>";
-      echo "<div class= 'float-right' id='paypal-button'></div>";
+
+      echo "<div class='float-right' id='paypal-button'></div>";
     ?>
+  </div>
 </div>
 
-<!--
-JavaScript code to render PayPal checkout button
-and execute payment
--->
-
-<script>
-paypal.Button.render({
-    // Configure environment
-    env: '<?php echo $paypal->paypalEnv; ?>',
-    client: {
-        sandbox: '<?php echo $paypal->paypalClientID; ?>',
-        production: '<?php echo $paypal->paypalClientID; ?>'
-    },
-    // Customize button (optional)
-    locale: 'en_GB',
-    style: {
-        size: 'medium',
-        color: 'gold',
-        textarea: 'white',
-        shape: 'pill'
-    },
-    // Set up a payment
-    payment: function (actions) {
-        return actions.payment.create({
-            transactions: [{
-                amount: {
-                    total: '<?php echo $totalCost?>',
-                    currency: 'GBP'
-                }
-            }]
-      });
-    },
-
-    // Execute the payment
-    onAuthorize: function (data, actions) {
-        return actions.payment.execute()
-        .then(function () {
-            // Show a confirmation message to the buyer
-            window.alert('Thank you for your purchase!');
-            // Redirect to the payment process page
-            window.location = "../Controller/process.php?paymentID="+data.paymentID+"&token="+data.paymentToken+"&payerID="+data.payerID;
-        });
-    }
-}, '#paypal-button');
-</script>
+<?php
+include '../Controller/payPalButton.php';
+include '../Controller/bootstrapScript.php';
+?>
 </body>
 </html>
