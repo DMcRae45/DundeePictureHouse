53a181550cc2f4914c6060aecd799f10846004ff
aaronhay92 <1800855@uad.ac.uk>
2019-03-29 17:36:23 +0000

    Updated independent info page.

---
+++ View/independentInfo.php
@@ -0,0 +1,28 @@
+<?php
+/*
+    Description: Information page on idependent movies, and how to contact cinema.
+    Author: Aaron Hay
+*/
+?>
+<html>
+<!--<head>-->
+    <?php
+        include 'header.php';
+    ?>
+<!-- </head> -->
+<title>DPH - Independent Movies Information</title>
+  <body>
+
+  <div class="container">
+    <h1>#MakingMovies</h1>
+    <p>As a recently established cinema, we realise it is not easy for a project to get off the ground. Knowing this, we are reaching out to budding film-makers and local talent to showcase their movies at our cinema.</p>
+    <p>We are looking for imaginative and creative works that exhibit the meaning of independent cinema.</p>
+    <p>All enquiries can be made at independentmovies@dphenquiries.com./p>
+    <p>Terms and conditions apply.</p>
+  </div>
+
+  <?php
+    include 'footer.php';
+   ?>
+ </body>
+</html>
