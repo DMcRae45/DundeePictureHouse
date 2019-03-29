257d7c9b55bd299e846a06feadb18bbf28080d7b
DMcRae45 <DMcRae45@gmail.com>
2019-03-29 21:32:57 +0000

    get UserTicket controller file updated

--- Controller/getUserTicket.php
+++ Controller/getUserTicket.php
@@ -2,10 +2,12 @@
 /*
     Description: User interface used to manage old and current movie tickets.
 
-    Author: Brad Mair
+    Author: Brad Mair, David McRae
 */
 include '../Model/DPH-api.php';
 
-$Tickets = GetUserTickets($sessionid);
-$ticketArray = json_decode($Tickets);
+$userid = $_SESSION['userid'];
+
+$Tickets = GetUserTickets($userid);
+$userTicketArray = json_decode($Tickets);
 ?>
