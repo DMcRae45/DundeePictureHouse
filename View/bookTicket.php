7979525e5e673d9a8f5efef9ea93048f63028afb
DMcRae45 <DMcRae45@gmail.com>
2019-03-29 23:31:37 +0000

    book ticket page now requires users to be logged in.

--- View/bookTicket.php
+++ View/bookTicket.php
@@ -3,65 +3,66 @@
     Description: Dundee Picture House Booking page for users to purchase tickets.
     Author: David McRae, Brad Mair
 */
-?>
-<html>
-<!--<head>-->
-<?php
 include 'header.php';
 include '../Controller/getTicketInfo.php';
-
-$ticketTypeString = "";
-$comma = ",";
-$quote = '"';
-for ($i=0 ; $i < sizeof($priceArray) ; $i++)
+if (!isset($_SESSION['LoggedIn']))
 {
-  if($i < sizeof($priceArray) -1)
-  {
-    $ticketTypeString = $ticketTypeString.$quote.strtolower($priceArray[$i]->Ticket_Type).$quote.$comma;
-  }
-  else
-  {
-    $ticketTypeString = $ticketTypeString.$quote.strtolower($priceArray[$i]->Ticket_Type).$quote;
-  }
+  header("Location: index.php");
 }
+else
+{
+echo "<html>";
+
+  $ticketTypeString = "";
+  $comma = ",";
+  $quote = '"';
+  for ($i=0 ; $i < sizeof($priceArray) ; $i++)
+  {
+    if($i < sizeof($priceArray) -1)
+    {
+      $ticketTypeString = $ticketTypeString.$quote.strtolower($priceArray[$i]->Ticket_Type).$quote.$comma;
+    }
+    else
+    {
+      $ticketTypeString = $ticketTypeString.$quote.strtolower($priceArray[$i]->Ticket_Type).$quote;
+    }
+  }
 
 
-echo "<div class='container'>"; // Open container
-  echo "<div class='mt-4'>";
-    echo "<h1 class='d-inline'>".$movieArray->Title."</h1>"; // Display movie title
-    echo "<img class='d-inline float-right' src=".$movieArray->Age_Rating." class='img-fluid' style='height: 3rem'>";
-  echo "</div>";
-    echo "<div class='row mt-4'>";
-      echo "<div class='col-md-6'>";
-        echo "<p>Release Date: <text>".$movieArray->Release_Date."</text></p>";
-
-        echo "<p>Runtime: <text>".$movieArray->RunTime." Mins</text></p>";
-        echo "<hr>";
-        echo "<img src=".$movieArray->Star_Rating." class='img-fluid' style='height: 2rem'>";
-        echo "<hr>";
-        echo "<p>Showing Time: <text>".$showingArray->Showing_Start_Time."</text></p>";
-        echo "<hr>";
-        echo "<p>Screen: <text>".$showingArray->Screen_ID."</text></p>";
-        echo "<hr>";
-      echo "</div>";
+  echo "<div class='container'>"; // Open container
+    echo "<div class='mt-4'>";
+      echo "<h1 class='d-inline'>".$movieArray->Title."</h1>"; // Display movie title
+      echo "<img class='d-inline float-right' src=".$movieArray->Age_Rating." class='img-fluid' style='height: 3rem'>";
     echo "</div>";
-echo "</div>"; // Close container
+      echo "<div class='row mt-4'>";
+        echo "<div class='col-md-6'>";
+          echo "<p>Release Date: <text>".$movieArray->Release_Date."</text></p>";
+          echo "<p>Runtime: <text>".$movieArray->RunTime." Mins</text></p>";
+          echo "<hr>";
+          echo "<img src=".$movieArray->Star_Rating." class='img-fluid' style='height: 2rem'>";
+          echo "<hr>";
+          echo "<p>Showing Time: <text>".$showingArray->Showing_Start_Time."</text></p>";
+          echo "<hr>";
+          echo "<p>Screen: <text>".$showingArray->Screen_ID."</text></p>";
+          echo "<hr>";
+        echo "</div>";
+      echo "</div>";
+  echo "</div>"; // Close container
 
-?>
+echo "
+  <body>
+  <div class='container table-responsive'>
+    <table class='table border border-dark text-center mt-4'>
+      <form method='POST' action='checkout.php?showingid=".$showingID."'>
 
-<body>
-<div class="container table-responsive">
-  <table class='table border border-dark text-center mt-4'>
-    <?php echo "<form method='POST' action='checkout.php?showingid=".$showingID."'>";?>
-
-      <thead class='thead-dark'>
-        <tr>
-          <th scope='col'>Ticket Type</th>
-          <th scope='col'>Showing Type</th>
-          <th scope='col'>Quantity</th>
-        </tr>
-      </thead>
-      <?php
+        <thead class='thead-dark'>
+          <tr>
+            <th scope='col'>Ticket Type</th>
+            <th scope='col'>Showing Type</th>
+            <th scope='col'>Quantity</th>
+          </tr>
+        </thead>
+";
       for ($i=0 ; $i < sizeof($priceArray) ; $i++)
       {
         echo "
@@ -104,22 +105,26 @@ echo "</div>"; // Close container
         </tr>
         ";
       }
-      ?>
-      <tr>
-        <td><?php echo "<h6>Total Cost: </h6>"; ?></td>
-        <td></td>
-        <td><?php echo "<h6 id='totalCost'>£0</h6>"; ?></td>
-      </tr>
-      <tr>
-        <td colspan="3"><button class='btn btn-info btn-block'>Checkout</button></td>
-      </tr>
-    </form>
-  </table>
-</div>
-<script src='../Controller/calculatePrice.js'></script>
-<!-- <footer> -->
-        <?php include 'footer.php'; ?>
-<!-- </footer> -->
-    <?php include '../Controller/bootstrapScript.php'; ?>
+      echo "
+          <tr>
+            <td><h6>Total Cost: </h6></td>
+            <td></td>
+            <td><h6 id='totalCost'>£0</h6></td>
+          </tr>
+          <tr>
+            <td colspan='3'><button class='btn btn-info btn-block'>Checkout</button></td>
+          </tr>
+        </form>
+      </table>
+    </div>
+    <script src='../Controller/calculatePrice.js'></script>
+    ";
+
+include 'footer.php';
+include '../Controller/bootstrapScript.php';
+echo "
 </body>
 </html>
+";
+}
+?>
