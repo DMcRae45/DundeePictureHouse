e8b9af5d02add1ad214047cd495e8d8bc21055cb
DMcRae45 <DMcRae45@gmail.com>
2019-03-30 01:01:58 +0000

    fixed table dsplay issues on the userTicket page.

--- View/userTicket.php
+++ View/userTicket.php
@@ -7,32 +7,41 @@
 include 'header.php';
 include '../Controller/getUserTicket.php';
 
- if(!$_SESSION['LoggedIn'] == true)
- {
-  header("Location:../View/index.php");
- }
- else
- {
+if(!$_SESSION['LoggedIn'] == true)
+{
+header("Location:../View/index.php");
+}
+else
+{
 echo "<body>";
 echo "<div class='container'>"; // Open container
   echo "<div class='mt-4'>";
-    echo "<h3 class='d-inline'>".$userTicketArray->Title."</h3>"; // Display movie title
+    echo "<h3>Showing At: <text>Dundee Picture House</text></h3>";
   echo "</div>";
+
+echo "<div class='row mt-4 mx-auto'>";
+  echo "<h3>Title: <text>".$userTicketArray[0]->Title."</text></h3>"; // Display movie title
+  echo "<hr>";
+echo "</div>";
+
     echo "<div class='row mt-4 mx-auto'>";
       echo "<div class='col-md-3'>";
-        echo "<p>Showing At: <text>Dundee Picture House</text></p>";
-        echo "<hr>";
-        echo "<p>Starting At: <text>".$userTicketArray->Showing_Start_Time."</text></p>";
-        echo "<hr>";
-        echo "<p>Showing In: <text>".$userTicketArray->Showing_Type."</text></p>";
-        echo "<hr>";
-      echo "</div>";
 
+        echo "<p>Starting At: <text>".$userTicketArray[0]->Showing_Start_Time."</text></p>";
+        echo "<hr>";
+
+        echo "<p>Showing In: <text>".$userTicketArray[0]->Showing_Type."</text></p>";
+        echo "<hr>";
+
+      echo "</div>";
       echo "<div class='col-md-3'>";
-        echo "<p>Date: <text>".$userTicketArray->Showing_Date."</text></p>";
+
+        echo "<p>Date: <text>".$userTicketArray[0]->Showing_Date."</text></p>";
         echo "<hr>";
-        echo "<p>Screen: <text>".$userTicketArray->Screen_ID."</text></p>";
+
+        echo "<p>Screen: <text>".$userTicketArray[0]->Screen_ID."</text></p>";
         echo "<hr>";
+
       echo "</div>";
     echo "</div>";
 
@@ -41,12 +50,11 @@ echo "</div>"; // Close container
 
 
 echo "
-  <body>
-  <div>
-  <text>Ticket For Your Next Showing</text>
-  </div>
   <div class='container table-responsive'>
     <table class='table border border-dark text-center mt-4'>
+      <div class='container'>
+        <text>Ticket For Your Next Showing</text>
+      </div>
         <thead class='thead-dark'>
           <tr>
             <th scope='col'>Ticket Code</th>
@@ -99,65 +107,61 @@ echo "
           $index++;
           $latestCode = $userTicketArray[$index]->Code;
         }
+        echo "</table></div>";
 
 echo "
-<div>
-<text>UpComing Showings</text>
-</div>
-        <div class='container table-responsive'>
-          <table class='table border border-dark text-center mt-4'>
+<div class='container table-responsive'>
+  <div class='container'>
+    <text>UpComing Showings</text>
+  </div>
+<table class='table border border-dark text-center mt-4'>
+    <thead class='thead-dark'>
+      <tr>
+        <th scope='col'>Ticket Code</th>
+        <th scope='col'>Movie</th>
+        <th scope='col'>Ticket Type</th>
+        <th scope='col'>Seating Type</th>
+        <th scope='col'>Date</th>
+        <th scope='col'>Time</th>
+      </tr>
+    </thead>
+";
+for($index = $index; $index < sizeof($userTicketArray) ; $index++)
+{
+  echo "
+    <tr>
 
-              <thead class='thead-dark'>
-                <tr>
-                  <th scope='col'>Ticket Code</th>
-                  <th scope='col'>Screen</th>
-                  <th scope='col'>Movie</th>
-                  <th scope='col'>Ticket Type</th>
-                  <th scope='col'>Seating Type</th>
-                  <th scope='col'>Date</th>
-                  <th scope='col'>Time</th>
-                </tr>
-              </thead>
-      ";
-              for($index = $index; $index < sizeof($userTicketArray) ; $index++)
-              {
-                echo "
-                  <tr>
+      <td>
+        <h6>".$userTicketArray[$index]->Code."</h6>
+      </td>
 
-                    <td>
-                      <h6>".$userTicketArray[$index]->Code."</h6>
-                    </td>
+      <td>
+        <text>".$userTicketArray[$index]->Title."</text>
+      </td>
 
-                    <td>
-                      <text>".$userTicketArray[$index]->Screen_ID."</text>
-                    </td>
+      <td>
+        <text>".$userTicketArray[$index]->Ticket_Type."</text>
+      </td>
 
-                    <td>
-                      <text>".$userTicketArray[$index]->Title."</text>
-                    </td>
+      <td>
+        <text>".$userTicketArray[$index]->Seating_Type."</text>
+      </td>
 
-                    <td>
-                      <text>".$userTicketArray[$index]->Ticket_Type."</text>
-                    </td>
+      <td>
+        <text>".$userTicketArray[$index]->Showing_Date."</text>
+      </td>
 
-                    <td>
-                      <text>".$userTicketArray[$index]->Seating_Type."</text>
-                    </td>
+      <td>
+        <text>".$userTicketArray[$index]->Showing_Start_Time."</text>
+      </td>
 
-                    <td>
-                      <text>".$userTicketArray[$index]->Showing_Date."</text>
-                    </td>
-
-                    <td>
-                      <text>".$userTicketArray[$index]->Showing_Start_Time."</text>
-                    </td>
-
-                  </tr>
-                ";
-              }
+    </tr>
+  ";
+}
+echo "</table></div>";
 
 
-include 'footer.php';
+include '../View/footer.php';
 include '../Controller/bootstrapScript.php';
 
 echo "
